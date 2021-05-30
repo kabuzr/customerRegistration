2c33c6b5d636db259e8e8322dfa0fc70a038cfa0
Abuzer <khanabuzr1999@gmail.com>
2021-05-30 18:27:18 +0530

    3rd commit without auto increment

--- src/controller/Registration.java
+++ src/controller/Registration.java
@@ -17,21 +17,19 @@ import javax.servlet.http.HttpServletResponse;
 		
 	}*/
    
- 
-	private static final long serialVersionUID = 1L;
+ private static final long serialVersionUID = 1L;
 
-protected void doPost(HttpServletRequest request ,HttpServletResponse response) throws ServletException,IOException {
+ protected void doPost(HttpServletRequest request ,HttpServletResponse response) throws ServletException,IOException {
 	response.setContentType("text/html");   
 	
 	
-
     String url="jdbc:mysql://localhost:3306/db2";
     String uname="root";
     String pass="root";
 
 	
 		try {
-			long partyid=  Long.parseLong(request.getParameter("partyid"));
+			int partyid=  Integer.parseInt(request.getParameter("partyid"));
 			String firstname= request.getParameter("firstname");
 			
 			String lastname= request.getParameter("lastname");
@@ -53,16 +51,16 @@ protected void doPost(HttpServletRequest request ,HttpServletResponse response) 
 		
             
 	        
-		     PreparedStatement	prs=  conn.prepareStatement("insert into party " + "values(?,?,?,?,?,?,?,?)");
-		     prs.setLong(1,partyid);
-		     prs.setString(1,firstname);
-		     prs.setString(2,lastname);
-		     prs.setString(3,address);
-		     prs.setString(4,city);
-		     prs.setInt(5,zip);
-		     prs.setString(6,state);
-		     prs.setString(7,country);
-		     prs.setInt(8,phone);
+		     PreparedStatement	prs=  conn.prepareStatement("insert into party " + "values(?,?,?,?,?,?,?,?,?)");
+		     prs.setInt(1,partyid);
+		     prs.setString(2,firstname);
+		     prs.setString(3,lastname);
+		     prs.setString(4,address);
+		     prs.setString(5,city);
+		     prs.setInt(6,zip);
+		     prs.setString(7,state);
+		     prs.setString(8,country);
+		     prs.setInt(9,phone);
 		     
 		     PreparedStatement	prs2=  conn.prepareStatement("insert into userlogin " + "values(?,?,?)");
 		     prs2.setString(1,userloginid);
@@ -76,28 +74,7 @@ protected void doPost(HttpServletRequest request ,HttpServletResponse response) 
 		     
 		     out.println("Inserted");
 	
-   
-		
-		
-		
-		
-		
-		
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		}
+  }
 		 catch (ClassNotFoundException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
